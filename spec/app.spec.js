ee36197ed7103768f929526e9d323fb668cffc6b
Carter Gruss <cwgruss@gmail.com>
2017-10-07 22:01:20 -0500

    Edited the arrangement of the main spec file so that it used Jasmine's beforeEach statement, and added a test for selection sort.

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,7 +1,6 @@
 import {Sort, Util} from '../app/src/index';
 
 describe('Main App', () => {
-    console.log('====== SORTING-JS ======');
     describe('App Initialization', () => {
         it('should be able to be instantiated', () => {
             const sort = new Sort();
@@ -9,16 +8,28 @@ describe('Main App', () => {
         });
     });
 
-    describe('Sorting', () => {
+});
 
-        describe('Bubble Sort', () => {
-            it('should be able to sort a random array', () => {
-                const randomArray = Util.generateRandomArray(10, 10);
-                const sortable = new Sort(randomArray);
-                const result = sortable.bubbleSort((item1, item2) => item1 < item2);
-                expect(result).toEqual(randomArray.sort());
-            });
-        });
-        
+
+describe('A random array', () => {
+    var randomArray = null;
+    var sortable = null;
+    beforeEach(() => {
+        randomArray = Util.generateRandomArray(10, 10);
+        sortable = new Sort(randomArray);
+    });
+    afterEach(() => {
+        randomArray = null;
+        sortable = null;
+    });
+
+    it('can be sorted with Bubble Sort', ()=>{
+        const result = sortable.bubbleSort((item1, item2) => item1 < item2);
+        expect(result).toEqual(randomArray.sort());
+    });
+
+    it('can be sorted with Selection Sort', () => {
+        const result = sortable.selectionSort((item1, item2) => item1 < item2);
+        expect(result).toEqual(randomArray.sort());
     });
 });
