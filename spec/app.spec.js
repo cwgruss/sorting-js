413ba4a6418e71bdacf74f54f16d218c6ad8f6f7
Carter Gruss <cwgruss@gmail.com>
2017-10-07 22:53:30 -0500

    Added unit tests for Quick Sort, and made sure that when using Array.sort() that you specify a compare function

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -13,23 +13,71 @@ describe('Main App', () => {
 
 describe('A random array', () => {
     var randomArray = null;
+    var sortedArray = null;
     var sortable = null;
     beforeEach(() => {
-        randomArray = Util.generateRandomArray(10, 10);
+        let tmp = Util.generateRandomArray(10, 10);
+        randomArray = tmp.slice();
+        sortedArray = tmp.sort((a,b)=>{
+            return a - b;
+        });
         sortable = new Sort(randomArray);
     });
     afterEach(() => {
         randomArray = null;
+        sortedArray = null;
         sortable = null;
     });
 
     it('can be sorted with Bubble Sort', ()=>{
         const result = sortable.bubbleSort((item1, item2) => item1 < item2);
-        expect(result).toEqual(randomArray.sort());
+        expect(result).toEqual(sortedArray);
     });
 
     it('can be sorted with Selection Sort', () => {
         const result = sortable.selectionSort((item1, item2) => item1 < item2);
-        expect(result).toEqual(randomArray.sort());
+        expect(result).toEqual(sortedArray);
+    });
+
+    it('can be sorted with Quick Sort', () => {
+        const result = sortable.quickSort((item1, item2) => item1 < item2);
+        expect(result).toEqual(sortedArray);
+    });
+});
+
+
+describe('A unique array', () => {
+    var randomArray = null;
+    var sortedArray = null;
+    var sortable = null;
+    beforeEach(() => {
+        let tmp = Util.generateUniqueArray(1,25);
+        randomArray = tmp.slice();
+        sortedArray = tmp.sort((a,b)=>{
+            return a-b;
+        });
+        console.log(randomArray);
+        console.log(sortedArray);
+        sortable = new Sort(randomArray);
+    });
+    afterEach(() => {
+        randomArray = null;
+        sortedArray = null;
+        sortable = null;
+    });
+
+    it('can be sorted with Bubble Sort', () => {
+        const result = sortable.bubbleSort((item1, item2) => item1 < item2);
+        expect(result).toEqual(sortedArray);
+    });
+
+    it('can be sorted with Selection Sort', () => {
+        const result = sortable.selectionSort((item1, item2) => item1 < item2);
+        expect(result).toEqual(sortedArray);
+    });
+
+    it('can be sorted with Quick Sort', () => {
+        const result = sortable.quickSort((item1, item2) => item1 < item2);
+        expect(result).toEqual(sortedArray);
     });
 });
