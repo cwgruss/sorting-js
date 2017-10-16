c407c7bf71499cf6b75f19cbfe57bdf2360b98fe
Carter Gruss <cwgruss@gmail.com>
2017-10-15 21:05:23 -0500

    Added documentation to bubble sort function

--- app/src/sort/bubble-sort.js
+++ app/src/sort/bubble-sort.js
@@ -3,22 +3,31 @@ import Util from '../util/util';
 /**
  * Bubble Sort:
  * @param  {function} compareFunction Specifies a function that defines the sort order
- * @return {Array} result The sorted array.
+ * @return {Array} the sorted array
  */
-function bubbleSort(arr) {
+function bubbleSort(array) {
     return function (compareFunction) {
-        /* Copy the original array to avoid side effects. */
-        let result = arr.slice(0);
+        let result = array.slice(0); // Copy the array
 
-        let i = 0;
+        let i;
+
+        /* Until the array can be traversed in its entirity without
+         * swapping two elements, then there remain elements that
+         * need to be sorted. The varaible 'swapped' tracks whether
+         * two elements have had two be swapped. */
         let swapped = null;
 
         do {
+
             swapped = false;
+
+            /* Loop through the array, comparing the current element, array[i],
+             * with the next, array[i+1]. */
             for (i = 0; i < this.length - 1; i += 1) {
-                /* If the compareFunction(a,b) shows that items are in the
-                 * wrong order, swap the two elements and continue looping
-                 * through the array. */
+
+                /* If the compareFunction shows that the current element and
+                 * the next element are in wrong order, swap the two elements
+                 * and continue looping through the array. */
                 if (compareFunction(result[i + 1], result[i])) {
                     result = Util.swap(result, i, i + 1);
                     swapped = true;
@@ -28,7 +37,8 @@ function bubbleSort(arr) {
             /* If no two elements were swapped inside the inner
              * loop, then break */
         } while (swapped);
-        return result;
+
+        return result; // Return the sorted array
     };
 }
 
