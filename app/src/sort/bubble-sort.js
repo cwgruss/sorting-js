6982f17fb095ac47952457acc7e2b618590e5a29
Carter Gruss <cwgruss@gmail.com>
2017-11-20 20:04:30 -0600

    Refactored Bubble Sort to include a generator function, and moved tests to seperate bubble-sort.spec file

--- app/src/sort/bubble-sort.js
+++ app/src/sort/bubble-sort.js
@@ -1,15 +1,16 @@
 import Util from '../util/util';
 
-/**
- * Bubble Sort:
- * @param  {function} compareFunction Specifies a function that defines the sort order
- * @return {Array} the sorted array
- */
-function bubbleSort(array) {
-    return function (compareFunction) {
-        let result = array.slice(0); // Copy the array
+const BubbleSort = (function () {
+    /**
+     * Bubble Sort:
+     * @param  {function} compareFunction Specifies a function that defines the sort order
+     * @return {Array} the sorted array
+     */
+    const bubbleSort = function (compareFunction) {
+        let result = this.slice(0); // Copy the array
+        const lastIndex = result.length - 1;
 
-        let i;
+        let currentIndex;
 
         /* Until the array can be traversed in its entirity without
          * swapping two elements, then there remain elements that
@@ -18,18 +19,16 @@ function bubbleSort(array) {
         let swapped = null;
 
         do {
-
             swapped = false;
 
             /* Loop through the array, comparing the current element, array[i],
              * with the next, array[i+1]. */
-            for (i = 0; i < this.length - 1; i += 1) {
-
+            for (currentIndex = 0; currentIndex < lastIndex; currentIndex += 1) {
                 /* If the compareFunction shows that the current element and
                  * the next element are in wrong order, swap the two elements
                  * and continue looping through the array. */
-                if (compareFunction(result[i + 1], result[i])) {
-                    result = Util.swap(result, i, i + 1);
+                if (compareFunction(result[currentIndex + 1], result[currentIndex])) {
+                    result = Util.swap(result, currentIndex, currentIndex + 1);
                     swapped = true;
                 }
             }
@@ -40,6 +39,49 @@ function bubbleSort(array) {
 
         return result; // Return the sorted array
     };
-}
 
-export default bubbleSort;
+    const bubbleSortGenerator = function* (compareFunction) {
+        let result = this.slice(0); // Copy the array
+        const lastIndex = result.length - 1;
+        let currentIndex;
+
+        /* Until the array can be traversed in its entirity without
+         * swapping two elements, then there remain elements that
+         * need to be sorted. The varaible 'swapped' tracks whether
+         * two elements have had two be swapped. */
+        let swapped = null;
+
+        do {
+            swapped = false;
+
+            /* Loop through the array, comparing the current element, array[i],
+             * with the next, array[i+1]. */
+            for (currentIndex = 0; currentIndex < lastIndex; currentIndex += 1) {
+                /* If the compareFunction shows that the current element and
+                 * the next element are in wrong order, swap the two elements
+                 * and continue looping through the array. */
+                if (compareFunction(result[currentIndex + 1], result[currentIndex])) {
+                    result = Util.swap(result, currentIndex, currentIndex + 1);
+                    swapped = true;
+                    yield result;
+                }
+            }
+
+            /* If no two elements were swapped inside the inner
+             * loop, then break */
+        } while (swapped);
+
+        yield result;
+    };
+
+    return {
+        sort(compareFunction, useGenerator = false) {
+            if (useGenerator) {
+                return bubbleSortGenerator.call(this, compareFunction);
+            }
+            return bubbleSort.call(this, compareFunction);
+        },
+    };
+}());
+
+export default BubbleSort;
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,22 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './app.js',
+    entry: ['babel-polyfill', './app.js'],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     devtool: 'inline-source-map',
     module: {
-        rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }],
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({ 
-            sourceMap: true 
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
         }),
     ],
 };
