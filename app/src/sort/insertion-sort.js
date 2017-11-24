a9cf43486e13efa723ac7dcc058848a1af5ae2c9
Carter Gruss <cwgruss@gmail.com>
2017-11-24 13:50:42 -0600

    Refactored Insertion sort to also have a generator function

--- app/src/sort/insertion-sort.js
+++ app/src/sort/insertion-sort.js
@@ -1,57 +1,55 @@
 import Util from '../util/util';
 
-/**
- * Insert() inserts a given element——i.e. 'value'——into its
- * correct place in the left-hand, sorted partition of 'array';
- * the sorted parition is the subrarry[0..rightIndex). In order to do this, the
- * function makes room for 'value' by moving items that come after value to the right.
- *
- * @param {*} array 
- * @param {*} rightIndex 
- * @param {*} value 
- * @param {*} compareFunction 
- */
-const insert = (array, rightIndex, value, compareFunction) => {
-    let result = array.slice(0); // Copy the array
-
-    /* Starting at the right, at the end of the sorted parition, traverse
-     * the array (right to left) until an element that comes before 'value' is found, or the
-     * start of the array, and the sorted partition, is reached.  */
-    let i = rightIndex;
-    while ((!compareFunction(result[i], value)) && i >= 0) {
-
-        /* Make room for the element 'value' by shifting elements that
-         * come after 'value' to the right. */
-        result = Util.swap(result, i + 1, i);
-
-        i -= 1;
-    }
-
-    /* Finally, insert the value and return the partially sorted
-     * array.  */
-    result[i + 1] = value;
-    return result;
-};
-
-/**
- * Insertion Sort:
- * @param  {function} compareFunction Specifies a function that defines the sort order
- * @return {Array} result The sorted array.
- */
-function insertionSort(array) {
-    return function (compareFunction) {
+const InsertionSort = (function () {
+    /**
+     * Insert() inserts a given element——i.e. 'value'——into its
+     * correct place in the left-hand, sorted partition of 'array';
+     * the sorted parition is the subrarry[0..rightIndex). In order to do this, the
+     * function makes room for 'value' by moving items that come after value to the right.
+     *
+     * @param {*} array
+     * @param {*} rightIndex
+     * @param {*} value
+     * @param {*} compareFunction
+     */
+    const insert = (array, rightIndex, value, compareFunction) => {
         let result = array.slice(0); // Copy the array
+
+        /* Starting at the right, at the end of the sorted parition, traverse
+         * the array (right to left) until an element that comes before 'value' is found, or the
+         * start of the array, and the sorted partition, is reached.  */
+        let i = rightIndex;
+        while ((!compareFunction(result[i], value)) && i >= 0) {
+            /* Make room for the element 'value' by shifting elements that
+             * come after 'value' to the right. */
+            result = Util.swap(result, i + 1, i);
+            i -= 1;
+        }
+
+        /* Finally, insert the value and return the partially sorted
+         * array.  */
+        result[i + 1] = value;
+        return result;
+    };
+
+    /**
+     * Insertion Sort:
+     * @param  {function} compareFunction Specifies a function that defines the sort order
+     * @return {Array} result The sorted array.
+     */
+    const insertionSort = function (compareFunction) {
+        let result = this.slice(0); // Copy the array
         let valueToInsert;
 
 
         /* Loop over all the elements except the first one,
-         * which is assumed to be trivially sorted. One each consecutive
+         * which is assumed to be trivially sorted. On each consecutive
          * iteration, insert the current element in its correct position
          * in the sorted, left-hand partition. */
         let i = 1;
         let lastSortedIndex;
         while (i <= result.length - 1) {
-            valueToInsert = array[i]; // Grab the current element
+            valueToInsert = result[i]; // Grab the current element
 
             /* The previous element in the array is the last element in
              * the sorted array. */
@@ -64,6 +62,48 @@ function insertionSort(array) {
 
         return result; // Return the sorted array
     };
-}
 
-export default insertionSort;
+    /**
+     * Insertion Sort Generator:
+     * @param  {function} compareFunction Specifies a function that defines the sort order
+     * @yield {Array} the partially sorted array.
+     * @return {Array} the sorted array.
+     */
+    const insertionSortGenerator = function* (compareFunction) {
+        let result = this.slice(0); // Copy the array
+        let valueToInsert;
+
+
+        /* Loop over all the elements except the first one,
+         * which is assumed to be trivially sorted. On each consecutive
+         * iteration, insert the current element in its correct position
+         * in the sorted, left-hand partition. */
+        let i = 1;
+        let lastSortedIndex;
+        while (i <= result.length - 1) {
+            valueToInsert = result[i]; // Grab the current element
+
+            /* The previous element in the array is the last element in
+             * the sorted array. */
+            lastSortedIndex = i - 1;
+
+            // Insert the current element in its correct posistion on the left
+            result = insert(result, lastSortedIndex, valueToInsert, compareFunction);
+            i += 1;
+            yield result;
+        }
+        return result;
+    };
+
+    return {
+        sort(compareFunction, useGenerator = false) {
+            if (useGenerator) {
+                return insertionSortGenerator.call(this, compareFunction);
+            }
+            return insertionSort.call(this, compareFunction);
+        },
+    };
+}());
+
+
+export default InsertionSort;
---
+++ spec/sort/insertion-sort.spec.js
@@ -0,0 +1,155 @@
+import {
+    Sort,
+    Util,
+} from '../../app/src/index';
+
+describe('Insertion Sort\'s module', () => {
+    let randomArray = null;
+    let randomArrayCopy = null;
+    let arraySortedByJS = null;
+    let SortableArray = null;
+
+    describe(', given a random array of 10 non-unique numbers,', () => {
+        beforeEach(() => {
+            const tmp = Util.generateRandomArray(10, 10);
+            randomArray = tmp.slice(); // Copy the array
+            randomArrayCopy = tmp.slice(); // Copy the array, again
+            arraySortedByJS = tmp.sort((a, b) => a - b);
+            SortableArray = new Sort(randomArray);
+        });
+
+        afterEach(() => {
+            randomArray = null;
+            randomArrayCopy = null;
+            arraySortedByJS = null;
+            SortableArray = null;
+        });
+
+        it('returns a sorted array', () => {
+            const result = SortableArray.insertionSort((item1, item2) => item1 < item2, false);
+
+            expect(result).toEqual(arraySortedByJS);
+            expect(randomArray).toEqual(randomArrayCopy);
+        });
+
+        it('yields the steps to sorting the array', () => {
+            const generator = SortableArray.insertionSort((item1, item2) => item1 < item2, true);
+            let done = false;
+            let currentNext;
+            let currentNextValue;
+            while (!(currentNext = generator.next()).done) {;
+                currentNextValue = currentNext.value;
+            }
+            expect(currentNextValue).toEqual(arraySortedByJS);
+            expect(randomArray).toEqual(randomArrayCopy);
+        });
+    });
+
+    describe(', given a random array of 10 unique numbers,', () => {
+        beforeEach(() => {
+            const tmp = Util.generateUniqueArray(0, 10);
+            randomArray = tmp.slice(); // Copy the array
+            randomArrayCopy = tmp.slice(); // Copy the array, again
+            arraySortedByJS = tmp.sort((a, b) => a - b);
+            SortableArray = new Sort(randomArray);
+        });
+
+        afterEach(() => {
+            randomArray = null;
+            randomArrayCopy = null;
+            arraySortedByJS = null;
+            SortableArray = null;
+        });
+
+        it('returns a sorted array', () => {
+            const result = SortableArray.insertionSort((item1, item2) => item1 < item2, false);
+
+            expect(result).toEqual(arraySortedByJS);
+            expect(randomArray).toEqual(randomArrayCopy);
+        });
+
+        it('yields a sorted array', () => {
+            const generator = SortableArray.insertionSort((item1, item2) => item1 < item2, true);
+            let done = false;
+            let currentNext;
+            let currentNextValue;
+            while (!(currentNext = generator.next()).done) {;
+                currentNextValue = currentNext.value;
+            }
+            expect(currentNextValue).toEqual(arraySortedByJS);
+            expect(randomArray).toEqual(randomArrayCopy);
+        });
+    });
+
+    describe(', given a random array of 100 non-unique numbers,', () => {
+        beforeEach(() => {
+            const tmp = Util.generateRandomArray(100, 100);
+            randomArray = tmp.slice(); // Copy the array
+            randomArrayCopy = tmp.slice(); // Copy the array, again
+            arraySortedByJS = tmp.sort((a, b) => a - b);
+            SortableArray = new Sort(randomArray);
+        });
+
+        afterEach(() => {
+            randomArray = null;
+            randomArrayCopy = null;
+            arraySortedByJS = null;
+            SortableArray = null;
+        });
+
+        it('returns a sorted array', () => {
+            const result = SortableArray.insertionSort((item1, item2) => item1 < item2, false);
+
+            expect(result).toEqual(arraySortedByJS);
+            expect(randomArray).toEqual(randomArrayCopy);
+        });
+
+        it('yields a array sorted array', () => {
+            const generator = SortableArray.insertionSort((item1, item2) => item1 < item2, true);
+            let done = false;
+            let currentNext;
+            let currentNextValue;
+            while (!(currentNext = generator.next()).done) {;
+                currentNextValue = currentNext.value;
+            }
+            expect(currentNextValue).toEqual(arraySortedByJS);
+            expect(randomArray).toEqual(randomArrayCopy);
+        });
+    });
+
+    describe(', given a random array of 100 unique numbers,', () => {
+        beforeEach(() => {
+            const tmp = Util.generateUniqueArray(0, 100);
+            randomArray = tmp.slice(); // Copy the array
+            randomArrayCopy = tmp.slice(); // Copy the array, again
+            arraySortedByJS = tmp.sort((a, b) => a - b);
+            SortableArray = new Sort(randomArray);
+        });
+
+        afterEach(() => {
+            randomArray = null;
+            randomArrayCopy = null;
+            arraySortedByJS = null;
+            SortableArray = null;
+        });
+
+        it('returns a sorted array', () => {
+            const result = SortableArray.insertionSort((item1, item2) => item1 < item2, false);
+
+            expect(result).toEqual(arraySortedByJS);
+            expect(randomArray).toEqual(randomArrayCopy);
+        });
+
+        it('yields the steps to sorting the array', () => {
+            const generator = SortableArray.insertionSort((item1, item2) => item1 < item2, true);
+            let done = false;
+            let currentNext;
+            let currentNextValue;
+            while (!(currentNext = generator.next()).done) {;
+                currentNextValue = currentNext.value;
+            }
+            expect(currentNextValue).toEqual(arraySortedByJS);
+            expect(randomArray).toEqual(randomArrayCopy);
+        });
+    });
+});
